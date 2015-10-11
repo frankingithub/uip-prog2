78f447e2158a2f2377d1d9edf70f875ce2ecb2d1
frankingithub <franciscodj@gmail.com>
2015-10-11 18:39:04 -0500

    tareas added

---
+++ laboratorios/prueba 1/src/Perro.java
@@ -0,0 +1,16 @@
+
+public class Perro {
+	private String raza;
+	private int edad; 
+	private String color;
+	
+	public void ladrar() {
+		System.out.println("Estoy Ladrando");
+	}
+	public void comer() {
+		System.out.println("Estoy comer");
+	}
+	public void dormir() {
+		System.out.println("Estoy durmiendo");
+	}
+}
---
+++ laboratorios/prueba 1/src/Principal.java
@@ -0,0 +1,15 @@
+public class Principal {
+
+	public static void main(String[] args) {
+		System.out.println("Hola Mundo");
+		System.out.println("Hola Mundo");
+		
+		Perro lassie = new Perro();
+		lassie.comer();
+		lassie.ladrar();
+		lassie.dormir();
+		
+
+	}
+
+}
